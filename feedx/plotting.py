9ae346dfa520949ad310e057260ac5f909e0f6e6
gPS Team <no-reply@google.com>
2024-01-12 03:56:22 -0800

    No public description
    PiperOrigin-RevId: 597800279
    Change-Id: I22ea7da4865241446cd5d69d272bb5c7d259f5a9

--- feedx/plotting.py
+++ feedx/plotting.py
@@ -15,6 +15,7 @@
 """Plot historical and simulation data."""
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 
@@ -23,27 +24,71 @@ def _make_density_plot(
     column: str,
     ax: plt.Axes,
     x_label: str | None = None,
-    y_label: str | None = None
-    ) -> None:
-
+    y_label: str | None = None,
+) -> None:
   """Make a density plot using matplotlib visualizing distribution of the metric.
-  
+
   Args:
     data: data for plotting
     column: name of metric column in the data (eg clicks or impressions)
     ax: class containing the plotted data of the data set provided
     x_label: x-axis label. If not provided, column will be displayed
     y_label: y-axis label. If not provided, "Density" will be displayed
-
   """
   x_label = x_label or column
   y_label = y_label or "Density"
 
   ax.hist(
-      data[column], bins="auto", histtype="stepfilled", alpha=0.5, color="C0",
-      label=f'{x_label}')
+      data[column],
+      bins="auto",
+      histtype="stepfilled",
+      alpha=0.5,
+      color="C0",
+      label=f"{x_label}",
+  )
   ax.hist(data[column], bins=100, histtype="step", alpha=1.0, color="C0")
-  ax.set_title(f'Density Plot of {x_label} Distribution')
+  ax.set_title(f"Density Plot of {x_label} Distribution")
   ax.set_xlabel(x_label)
   ax.set_ylabel(y_label)
   ax.legend(title="Legend")
+
+
+def plot_metric_history(
+    data: pd.DataFrame,
+    metric_column: str,
+    week_start_column_name: str = "week_start",
+) -> np.ndarray:
+  """Plots the weekly distribution and the average of the metric per item per week.
+
+  Args:
+    data: data set to plot containing the week_start column and the metrics that
+      will be evaluated
+    metric_column: metric chosen to run the plot
+    week_start_column_name: column containing the week start date
+
+  Returns:
+    The axes containing the plots of the metric performance history
+  """
+
+  _, axs = plt.subplots(
+      nrows=1, ncols=2, figsize=(15, 5), constrained_layout=True
+  )
+
+  clean_metric_name = metric_column.replace("_", " ")
+
+  metric_time = (
+      data.groupby(week_start_column_name)[[metric_column]].sum().reset_index()
+  )
+  axs[0].plot(metric_time[week_start_column_name], metric_time[metric_column])
+  axs[0].set_title(f"Total {clean_metric_name} per week")
+  axs[0].set(xlabel="Start Week", ylabel=f"Total {clean_metric_name}")
+  axs[0].tick_params(axis="x", rotation=30)
+
+  _make_density_plot(
+      data,
+      ax=axs[1],
+      column=metric_column,
+      x_label=f"Average {clean_metric_name} per item per week",
+  )
+
+  return axs
--- feedx/plotting_test.py
+++ feedx/plotting_test.py
@@ -14,9 +14,63 @@
 
 """Tests for plotting."""
 
-import unittest
+from unittest import mock
+
+from absl.testing import absltest
+from absl.testing import parameterized
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 from feedx import plotting
 
+
+class PlotMetricHistoryTest(parameterized.TestCase):
+
+  def setUp(self):
+    super().setUp()
+    self.plot_data = pd.DataFrame({
+        "item_id": [1] * 6 + [2] * 6 + [3] * 6 + [4] * 6 + [5] * 6,
+        "week_start": pd.to_datetime(
+            [
+                "2023-10-02",
+                "2023-10-09",
+                "2023-10-16",
+                "2023-10-23",
+                "2023-10-30",
+                "2023-11-06",
+            ]
+            * 5
+        ),
+        "clicks": (
+            [0, 1, 5, 3, 2, 5]
+            + [0, 0, 0, 1, 0, 0]
+            + [5, 1, 0, 2, 5, 1]
+            + [2, 2, 1, 2, 4, 5]
+            + [9, 1, 2, 0, 0, 1]
+        ),
+        "impressions": (
+            [0, 1, 5, 3, 2, 5]
+            + [0, 0, 0, 1, 0, 0]
+            + [5, 1, 0, 2, 5, 1]
+            + [2, 2, 1, 2, 4, 5]
+            + [9, 1, 2, 0, 0, 1]
+        ),
+    })
+
+  def test_plot_metric_history_returns_array(self):
+    ax = plotting.plot_metric_history(self.plot_data, "impressions")
+    self.assertIsInstance(ax, np.ndarray)
+
+  def test_plot_metric_history_returns_2_axes(self):
+    ax = plotting.plot_metric_history(self.plot_data, "impressions")
+    self.assertTupleEqual(ax.shape, (2,))
+
+  def test_plot_metric_history_returns_array_of_axes(self):
+    ax = plotting.plot_metric_history(self.plot_data, "impressions")
+    self.assertIsInstance(ax[0], plt.Axes)
+    self.assertIsInstance(ax[1], plt.Axes)
+
+
+if __name__ == "__main__":
+  absltest.main()
